bb7dcf3687c541b4ef9e76224fb61f4f81a38e07
Yona211111112121212 <afanasevaa964@gmail.com>
2025-12-15 16:36:05 +0300

    Добавлены DevOps файлы: Docker Compose, мониторинг, Blue-Green

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import MainPage from './components/MainPage';
@@ -66,7 +66,7 @@ function App() {
     }
 
     return (
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
             <div className="App">
                 <Routes>
                     <Route path="/login" element={
